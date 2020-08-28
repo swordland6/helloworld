3f1bde6cc56bc470ec4c28dea2478443fc7d1acf
swordland6 <70335340+swordland6@users.noreply.github.com>
2020-08-28 20:17:25 +0800

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Drawing;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class2
+    {
+        public double FindMax(double a, double b)
+        {
+            double resrult;
+            if (a > b)
+                resrult = a;
+            else
+                resrult = b;
+            return resrult;
+        }
+        class Text
+        {
+            static void Main(string[] args)
+            {
+                double a,b;
+                Console.WriteLine("Pls enter a yours favourite number");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Pls enter again");
+                b = Convert.ToDouble(Console.ReadLine());
+                double ret;
+                Class2 n = new Class2();
+                ret = n.FindMax(a, b);
+                Console.WriteLine(/n"{0} IS MAX", ret);
+                Console.ReadKey();
+            }
+        }
+    }
+}
